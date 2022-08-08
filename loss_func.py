edb2d4879ce6c6cbc69fe2a3cb281a6dd03c05ff
卢君_AI <lujun@vocust.com>
2022-08-08 10:47:09 +0800

    ghmc loss

--- loss_func.py
+++ loss_func.py
@@ -720,6 +720,51 @@ class GHM(nn.Module):
         weights4examples /= torch.sum(weights4examples)
         return weights4examples * gradient  # return weighted gradients
 
+class GHM_Loss(nn.Module):
+    def __init__(self, bins, alpha):
+        super(GHM_Loss, self).__init__()
+        self._bins = bins
+        self._alpha = alpha
+        self._last_bin_count = None
+
+    def _g2bin(self, g):
+        return torch.floor(g * (self._bins - 0.0001)).long()
+
+    def _custom_loss(self, x, target, weight):
+        raise NotImplementedError
+
+    def _custom_loss_grad(self, x, target):
+        raise NotImplementedError
+
+    def forward(self, x, target):
+        g = torch.abs(self._custom_loss_grad(x, target))
+        bin_idx = self._g2bin(g)
+        bin_count = torch.zeros((self._bins))
+        for i in range(self._bins):
+            bin_count[i] = (bin_idx == i).sum().item()
+
+        N = x.size(0)
+
+        nonempty_bins = (bin_count > 0).sum().item()
+        gd = bin_count * nonempty_bins
+        gd = torch.clamp(gd, min=0.0001)
+        beta = N / gd
+        return self._custom_loss(x, target, beta[bin_idx])
+        
+class GHMC_Loss(GHM_Loss):
+    """与Focal Loss一样,GHM也是解决样本不平衡的利器
+    """
+    def __init__(self, bins, alpha):
+        super(GHMC_Loss, self).__init__(bins, alpha)
+
+    def _custom_loss(self, x, target, weight):
+        return torch.sum((torch.nn.NLLLoss(reduce=False)(torch.log(x),target)).mul(weight.to(target.device).detach()))/torch.sum(weight.to(device).detach())
+
+    def _custom_loss_grad(self, x, target):
+        x=x.cpu().detach()
+        target=target.cpu()
+        return torch.tensor([x[i,target[i]] for i in range(target.shape[0])])-target
+
 
 class WarmupLR(_LRScheduler):
     """The WarmupLR scheduler
