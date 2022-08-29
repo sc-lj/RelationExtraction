311ff234356619faae3412c4ea89c7179a8726dc
卢君_AI <lujun@vocust.com>
2022-08-29 19:25:32 +0800

    tdeer

---
+++ TDeer/__init__.py
@@ -0,0 +1,2 @@
+from .Dataset import TDEERDataset, collate_fn, collate_fn_val
+from .Model import TDEERPytochLighting
--- train.py
+++ train.py
@@ -123,7 +123,7 @@ def main():
     args = parser_args()
     if args.model_type == "tdeer":
         from TDeer.Model import TDEERPytochLighting
-        from TDeer.Dataset import TDEERDataset, collate_fn, collate_fn_val
+        from TDeer import TDEERDataset, collate_fn, collate_fn_val
         train_dataset = TDEERDataset(args.train_file, args, is_training=True)
         train_dataloader = DataLoader(train_dataset, collate_fn=collate_fn,
                                       batch_size=args.batch_size, shuffle=True, num_workers=8, pin_memory=True)
